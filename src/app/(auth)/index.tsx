bb391e7e6c3186bff4dc0ca83e02bdbf182c9d1f
dominic224 <dev007.js@gmail.com>
2025-01-18 17:47:53 +0700

    update

--- src/app/(auth)/index.tsx
+++ src/app/(auth)/index.tsx
@@ -1,5 +1,13 @@
 import React, { useState } from "react";
-import { Alert, AppState, Image, View } from "react-native";
+import {
+  Alert,
+  AppState,
+  Image,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { supabase } from "~/src/lib/supabase";
 import { images } from "~/src/utils/images";
 
@@ -50,6 +58,67 @@ export default function Auth() {
   return (
     <View className="flex flex-1 flex-col items-center justify-center bg-white">
       <Image source={images.logo} className="w-[182] h-[49]" />
+      <Image source={images.avatar} style={styles.avt} />
+      <Text style={styles.name}>jacob_w</Text>
+      <Pressable style={styles.button}>
+        <Text style={styles.login}>Log in</Text>
+      </Pressable>
+      <Text style={styles.switchAcc}>Switch accounts</Text>
+      <View style={styles.footer}>
+        <Text style={styles.footerTxt}>
+          Don't have an account?{" "}
+          <Text style={{ fontWeight: "600", color: "#000" }}>Sign up</Text>
+        </Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  avt: {
+    width: 85,
+    height: 85,
+    borderRadius: "50%",
+    marginTop: 35,
+  },
+  name: {
+    fontSize: 14,
+    fontWeight: "500",
+    marginTop: 10,
+  },
+  button: {
+    backgroundColor: "#3797EF",
+    width: "85%",
+    height: 50,
+    borderRadius: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 10,
+  },
+  login: {
+    color: "#fff",
+    fontSize: 14,
+    fontWeight: "600",
+  },
+  switchAcc: {
+    fontSize: 14,
+    color: "#3797EF",
+    marginTop: 30,
+    fontWeight: "500",
+  },
+  footer: {
+    position: "absolute",
+    bottom: 0,
+    borderTopWidth: 0.5,
+    borderColor: "gray",
+    paddingBottom: 60,
+    paddingTop: 30,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  footerTxt: {
+    fontSize: 12,
+    color: "gray",
+  },
+});
--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,3 +1,4 @@
 export const images = Object.freeze({
   logo: require(`~/assets/instagram-logo.png`),
+  avatar: require("~/assets/avatar.png"),
 });
